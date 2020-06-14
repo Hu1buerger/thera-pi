bb1df7f7d0f351a2c7fd3e82794a7e8888330b08
Keith <juthofrohe@wemel.top>
2020-06-14 10:00:00 +0200

    Added patiententable data

--- Files/tests/resources/sql/nummern.sql
+++ Files/tests/resources/sql/nummern.sql
@@ -1,2 +1,2 @@
 DELETE from nummern;
-INSERT INTO `nummern` VALUES (2,1,1,2,1,1,1,1,1,1,1,1,1,1,1,1,'123456789',0,2,1);
+INSERT INTO `nummern` VALUES (7,1,1,2,1,1,1,1,1,1,1,1,1,1,1,1,'123456789',0,2,1);
