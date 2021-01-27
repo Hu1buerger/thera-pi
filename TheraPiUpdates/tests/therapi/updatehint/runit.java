f3e2f9640bb7c5e5cbd3a9b99387f02ee98201ea
Astrid Sawatzky <astrid.sawatzky@gmx.de>
2021-01-27 10:45:43 +0100

    fix compile error in test

--- TheraPiUpdates/tests/therapi/updatehint/runit.java
+++ TheraPiUpdates/tests/therapi/updatehint/runit.java
@@ -1,11 +1,11 @@
 package therapi.updatehint;
 
 import therapi.updatehint.fx.UpdatesMainFX;
-import therapi.updatehint.swing.UpdatesMain;
+import therapi.updatehint.swing.UpdatesMainSwing;
 
 public class runit {
     public static void main(String[] args) {
-        HintMain updateCheck = new HintMain(new UpdatesMain());
+        HintMain updateCheck = new HintMain(new UpdatesMainSwing());
         updateCheck.execute();
     }
 
