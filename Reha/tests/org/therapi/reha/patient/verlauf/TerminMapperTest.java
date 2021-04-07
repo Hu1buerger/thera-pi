ab6fd0de8384e035a86715c2f27a07e857f4d44a
Astrid Sawatzky <astrid.sawatzky@gmx.de>
2021-04-07 10:08:49 +0200

    expand termintest

--- Reha/tests/org/therapi/reha/patient/verlauf/TerminMapperTest.java
+++ Reha/tests/org/therapi/reha/patient/verlauf/TerminMapperTest.java
@@ -31,4 +31,24 @@ public class TerminMapperTest {
         assertEquals(expectedTerminListe, terminListe);
     }
 
+    @Test
+    public void mehrereTermineamSelbenTagMitverschiedenenRezeptensindverschieden() throws Exception {
+        String behandler = "Katrin Ehring";
+        String terminString = "20.02.2019@" + behandler
+                + "@@54105@2019-02-20\n20.02.2019@Katrin Ehring@@54105@2019-02-27" + "\n"
+                + "06.03.2019@Katrin Ehring@@54105@2019-03-06" + "\n" + "13.03.2019@Katrin Ehring@@54105@2019-03-13";
+
+        int pat_id = 5;
+        String rezeptNr = "123";
+        TerminMapper mapper = new TerminMapper(terminString, pat_id, rezeptNr);
+
+        List<Termin> terminListe = mapper.termine();
+        assertEquals(4, terminListe.size());
+        List<Termin> expectedTerminListe = Arrays.asList(
+                new Termin(LocalDate.of(2019, 2, 20), behandler, pat_id, rezeptNr),
+                new Termin(LocalDate.of(2019, 2, 20), behandler, pat_id, rezeptNr),
+                new Termin(LocalDate.of(2019, 3, 6), behandler, pat_id, rezeptNr),
+                new Termin(LocalDate.of(2019, 3, 13), behandler, pat_id, rezeptNr));
+        assertEquals(expectedTerminListe, terminListe);
+    }
 }
